1ab5e100ed3b39cd8291feca7998869dba63fac0
Jeff Hillman <jeff.hillman@gmail.com>
2021-12-14 19:47:43 -0700

    Day 6

--- day06/part1.c
+++ day06/part1.c
@@ -1,71 +1,11 @@
 /* Day 6, part 1 = 372300 */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-struct LanternFish {
-    int timer;
-    struct LanternFish *next;
-};
+#include "lanternfish.h"
 
 int main() {
-    FILE *inputFile = fopen("input.txt", "r");
+    long answer = simulate(80);
 
-    if (inputFile) {
-        struct LanternFish *school = (struct LanternFish *)calloc(1, sizeof(struct LanternFish));
-        struct LanternFish *currentFish = school;
-        struct LanternFish *lastFish;
-        char ch;
-        int answer = 0;
-
-        while (fscanf(inputFile, "%d%c", &currentFish->timer, &ch) >= 1) {
-            lastFish = currentFish;
-
-            if (ch == ',') {
-                currentFish->next = (struct LanternFish *)calloc(1, sizeof(struct LanternFish));
-                currentFish = currentFish->next;
-            }
-
-            ch = '\0';
-        }
-
-        fclose(inputFile);
-        
-        for (int i = 0; i < 80; i++) {
-            currentFish = school;
-            int newFishCount = 0;
-
-            while (currentFish) {
-                if (currentFish->timer == 0) {
-                    currentFish->timer = 6;
-                    ++newFishCount;
-                } else {
-                    --currentFish->timer;
-                }
-
-                currentFish = currentFish->next;
-            }
-
-            while (newFishCount > 0) {
-                lastFish->next = (struct LanternFish *)calloc(1, sizeof(struct LanternFish));
-                lastFish = lastFish->next;
-                lastFish->timer = 8;
-
-                --newFishCount;
-            }
-        }
-
-        while (school) {
-            currentFish = school->next;
-
-            free(school);
-
-            ++answer;
-            school = currentFish;
-        }
-
-        printf("%d", answer);
-    }
+    printf("%ld", answer);
 
     return 0;
 }
--- day06/part2.c
+++ day06/part2.c
@@ -1,39 +1,11 @@
 /* Day 6, part 2 = 1675781200288 */
 
-#include <stdio.h>
+#include "lanternfish.h"
 
 int main() {
-    FILE *inputFile = fopen("input.txt", "r");
+    long answer = simulate(256);
 
-    if (inputFile) {
-        long timers[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-        long timer;
-        char ch;
-        long answer = 0;
-
-        while (fscanf(inputFile, "%ld%c", &timer, &ch) >= 1) {
-            ++timers[timer];
-        }
-
-        fclose(inputFile);
-        
-        for (int i = 0; i < 256; i++) {
-            long newFishCount = *timers;
-
-            for (timer = 0; timer < 8; timer++) {
-                timers[timer] = timers[timer + 1];
-            }
-
-            timers[6] += newFishCount;
-            timers[8] = newFishCount;
-        }
-
-        for (int i = 0; i < 9; i++) {
-            answer += timers[i];
-        }
-
-        printf("%ld", answer);
-    }
+    printf("%ld", answer);
 
     return 0;
 }
