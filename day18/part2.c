ed07c135e1c66353af9e7fd3383578029ee4a60e
Jeff Hillman <jeff.hillman@gmail.com>
2021-12-20 07:33:51 -0700

    Day 18

--- day18/part2.c
+++ day18/part2.c
@@ -1,16 +1,55 @@
-/* Day 18, part 2 = ? */
+/* Day 18, part 2 = 4819 */
 
-#include <stdio.h>
+#include "snailfish.h"
 
 int main() {
     FILE *inputFile = fopen("input.txt", "r");
 
     if (inputFile) {
-        int answer = 0;
+        int numberCount = 0;
+        char numberString[64];
+        struct SnailfishNumber **numbers;
+        long answer = 0;
+
+        while (fgets(numberString, sizeof(numberString), inputFile)) {
+            ++numberCount;
+        }
+
+        fseek(inputFile, 0, SEEK_SET);
+
+        numbers = (struct SnailfishNumber **)calloc(numberCount, sizeof(struct SnailfishNumber *));
+
+        for (int i = 0; i < numberCount; i++) {
+            fgets(numberString, sizeof(numberString), inputFile);
+
+            numbers[i] = readNumber(NULL, numberString, NULL);
+        }
 
         fclose(inputFile);
 
-        printf("%d", answer);
+        for (int i = 0; i < numberCount; i++) {
+            for (int j = 0; j < numberCount; j++) {
+                if (i != j) {
+                    struct SnailfishNumber *sum = add(copy(numbers[i], NULL), copy(numbers[j], NULL));
+
+                    long sumMagnitude = magnitude(sum);
+
+                    if (sumMagnitude > answer) {
+                        answer = sumMagnitude;
+                    }
+
+                    freeNumber(sum);
+                }
+            }
+        }
+
+        for (int i = 0; i < numberCount; i++) {
+            freeNumber(numbers[i]);
+        }
+
+        free(numbers);
+
+        printf("%ld", answer);
     }
 
     return 0;
