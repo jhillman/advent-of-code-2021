8485b2c332e373a496f00fe8edf2334b2bf65d3f
Jeff Hillman <jeff.hillman@gmail.com>
2021-12-22 07:41:28 -0700

    Part 2

--- day19/part2.c
+++ day19/part2.c
@@ -1,14 +1,26 @@
-/* Day 19, part 2 = ? */
+/* Day 19, part 2 = 19130 */
 
-#include <stdio.h>
+#include "scanners.h"
 
 int main() {
-    FILE *inputFile = fopen("input.txt", "r");
+    struct ScannerData *data = getScannerData();
 
-    if (inputFile) {
+    if (data) {
+        mapScanners(data);
+
         int answer = 0;
 
-        fclose(inputFile);
+        for (int i = 0; i < data->scannerCount; i++) {
+            for (int j = i + 1; j < data->scannerCount; j++) {
+                int scannerDistance = distance(data->scanners[i]->translation, data->scanners[j]->translation);
+
+                if (scannerDistance > answer) {
+                    answer = scannerDistance;
+                }
+            }
+        }
+
+        freeScannerData(data);
 
         printf("%d", answer);
     }
