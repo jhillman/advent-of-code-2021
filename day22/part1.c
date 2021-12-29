f4bb544f4d4c30e7f5e3340b8bace5e45658552e
Jeff Hillman <jeff.hillman@gmail.com>
2021-12-28 19:59:16 -0700

    Day 22

---
+++ day22/part1.c
@@ -0,0 +1,9 @@
+/* Day 22, part 1 = 590467 */
+
+#include "reactor.h"
+
+int main() {
+    printf("%ld", rebootReactor(true));
+
+    return 0;
+}
---
+++ day22/part2.c
@@ -0,0 +1,9 @@
+/* Day 22, part 2 = 1225064738333321 */
+
+#include "reactor.h"
+
+int main() {
+    printf("%ld", rebootReactor(false));
+
+    return 0;
+}
